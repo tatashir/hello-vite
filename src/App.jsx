2a5141f5d8c743a8b9e60c3f15693a0d1df33c00
tatashir <tshirotai9683@gmail.com>
2025-07-31 13:53:58 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello, GitHub Pages!</h1>
+      <p>This is my first Vite + React app on GitHub Pages 🚀</p>
+    </div>
+  );
+}
+
+export default App;
